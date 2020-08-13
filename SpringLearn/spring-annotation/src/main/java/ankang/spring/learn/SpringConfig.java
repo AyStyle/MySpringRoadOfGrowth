ce1c106f8000b02a7705d9b35805aa08c689b7ea
ankang <ankang@soyoung.com>
2020-08-13 21:18:43 +0800

    上传Spring注解开发学习笔记

--- SpringLearn/spring-annotation/src/main/java/ankang/spring/learn/SpringConfig.java
+++ SpringLearn/spring-annotation/src/main/java/ankang/spring/learn/SpringConfig.java
@@ -1,8 +1,10 @@
 package ankang.spring.learn;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
 
 /**
  * @author: ankang
@@ -14,6 +16,35 @@ import org.springframework.context.annotation.PropertySource;
 // @ComponentScan：配置包扫描
 @ComponentScan({"ankang.spring.learn"})
 // @PropertySource：引入外部资源文件
-@PropertySource("")
+@PropertySource("classpath:druid.properties")
+// @Import：引入其它Spring配置文件，统一使用该类导入
 public class SpringConfig {
+
+    // @Value：引入外部资源文件配置的变量
+    @Value("${druid.driver}")
+    private String driverClassName;
+    @Value("${druid.url}")
+    private String url;
+    @Value("${druid.username}")
+    private String username;
+    @Value("${druid.password}")
+    private String password;
+
+    /**
+     * 引入第三方bean实例对象
+     *
+     * @return
+     */
+    // @Bean：表明将方法返回的bean对象加入到SpringIOC管理中
+    @Bean("dataSource")
+    public DataSource createDataSource() {
+        final DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
 }
